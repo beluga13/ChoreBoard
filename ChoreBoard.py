43f22febe6393e88459593a710b1fb6633fc3a35
beluga13 <42304079+beluga13@users.noreply.github.com>
2019-02-02 17:31:37 +0000

    Update ChoreBoard.py

--- ChoreBoard.py
+++ ChoreBoard.py
@@ -4,17 +4,50 @@ import time
 def about():
     print("\n\tThis program is so awesome. Give full marks plz")
 
-def create():
-    print("\n\tPending")
+def creating():
+    HouseHold=str(input("Enter your household's name: "))
+    text=open(HouseHold+'.txt','w')
+    text.write("\nHousehold's Name: "+HouseHold)
 
+    Members=True
+    Counter=1
+    while str(Members) != "":
+        Members=str(input("Enter the name of participant" + str(Counter) + ": "))
+        if str(Members) != "":
+            text.write("\nParticipant " + str(Counter) + ": "  + Members)
+            Counter = Counter + 1
+
+    Chores=True
+    Counter=1
+    while str(Chores) != "":
+        Chores=str(input("Enter chore " + str(Counter) + ": "))
+        if Chores != "":
+            while True:
+                    try:
+                        Freq=int(input("What is the weekly frequency of this chore" + str(Counter) + ": "))
+                        while Freq < 0:
+                            print("Enter a positive frequency")
+                            Freq=int(input("What is the weekly frequency of this chore " + str(Counter) + ": "))
+                        text.write("\nChore: " + str(Chores) + "\n\t Times per week: " + str(Freq))
+                        Counter = Counter + 1
+                        break
+                    except ValueError:
+                        print("Enter a number")
+        
 def view():
-    print("\n\tPending")
+    search=str(input("Enter the household you would like to view: "))
+    try:
+        text=open(search+'.txt','r')
+        text_contents=text.read()
+        print(text_contents)
+    except:
+        print("\n\tThe househould you have entered does not exist")
 
 def log():
-    print("\n\tPending")
+    print("\n\tLogging scores function pending")
 
 def scores():
-    print("\n\tPending")
+    print("\n\tScoreboard pending")
 
 def quit():
     MenuLoop=False
@@ -41,7 +74,7 @@ def main():
             about()
 
         elif MenuOption.lower() == "c":
-            create()
+            creating()
 
         elif MenuOption.lower() == "v":
             view()
@@ -54,7 +87,10 @@ def main():
 
         elif MenuOption.lower() == "q":
             quit()
+
         else:
             print("\n\tInvalid input. Try again.")
 
 main()
+
+
